1dcfcfe01823c3102da44fdf6c0b59a2f17d585f
harik2 <mohamed.harik21@gmail.com>
2022-03-05 09:53:01 +0100

    formulaire ajout bd

---
+++ bd/store.php
@@ -0,0 +1,22 @@
+<?php 
+
+$libelle=$_POST['libelle'];
+$prix=$_POST['prix'];
+$qtestock=$_POST['qtestock'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+libelle : <?=$libelle?> <br>
+prix : <?=$_POST['prix']?> <br>
+libelle : <?=$qtestock?> <br>
+    
+</body>
+</html>
